43bab11747c2ae439f5d8b0b5987cef2c7231327
eduardo <eduardodelgado369@gmail.com>
2018-05-17 13:08:23 -0600

    Some advancement in the roi

--- preprocessing/video2frames.py
+++ preprocessing/video2frames.py
@@ -19,7 +19,8 @@ def video_to_frames(input_loc, output_loc):
     # Start capturing the feed
     cap = cv2.VideoCapture(input_loc)
     # Find the number of frames
-    video_length = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)) - 1
+    print(cv2.CAP_PROP_FRAME_COUNT)
+    video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1
     print ("Number of frames: ", video_length)
     count = 0
     print ("Converting video..\n")
@@ -31,7 +32,7 @@ def video_to_frames(input_loc, output_loc):
         cv2.imwrite(output_loc + "/%#05d.jpg" % (count+1), frame)
         count = count + 1
         # If there are no more frames left
-        if (count > (video_length-1)):
+        if (count > (video_length)):
             # Log the time again
             time_end = time.time()
             # Release the feed
