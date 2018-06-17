5eb94b215111efe21c6b01572604ae8ccd851e91
Omar Adrian Salinas Villanueva <omar.salinas1611@gmail.com>
2018-06-17 15:15:55 -0600

    Small work on generator, still not working

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup
 
 setup(
-    name             = 'Lipnet',
-    version          = '0.1',
-    license          = 'Apache License, Version 2.0',
-    long_description = open('README.md').read(),
-    url              = "http://www.github.com/omarsalinas16/lipnext",
-    packages         = ['lipnext', 'preprocessing', 'evaluation', 'training'],
-    zip_safe         = False,
-    install_requires = [
-        'dlib==19.8.1',
-        'h5py==2.7.1',
-        'imutils==0.4.6',
-        'Keras==2.1.6',
-        'matplotlib==2.2.2',
-        'numpy==1.14.3',
-        'opencv-python==3.4.1.15',
-        'Pillow==5.1.0',
-        'progress==1.3',
-        'scikit-image==0.13.1',
-        'scikit-video==1.1.10',
-        'scipy==1.1.0',
-        'tensorflow-gpu==1.8.0'
-    ]
+	name             = 'Lipnet',
+	version          = '0.1',
+	license          = 'Apache License, Version 2.0',
+	long_description = open('README.md').read(),
+	url              = "http://www.github.com/omarsalinas16/lipnext",
+	packages         = ['lipnext', 'preprocessing', 'evaluation', 'training'],
+	zip_safe         = False,
+	install_requires = [
+		'dlib==19.8.1',
+		'h5py==2.7.1',
+		'imutils==0.4.6',
+		'Keras==2.1.6',
+		'matplotlib==2.2.2',
+		'numpy==1.14.3',
+		'opencv-python==3.4.1.15',
+		'Pillow==5.1.0',
+		'progress==1.3',
+		'scikit-image==0.13.1',
+		'scikit-video==1.1.10',
+		'scipy==1.1.0',
+		'tensorflow-gpu==1.8.0'
+	]
 )
