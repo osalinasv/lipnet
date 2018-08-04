204e0d4fabaaa6f790fd51298f3de15ec30ec181
Omar Adrian Salinas Villanueva <omar.salinas1611@gmail.com>
2018-08-04 00:35:00 -0600

    Changed batch size back to 45

--- lipnext/helpers/video.py
+++ lipnext/helpers/video.py
@@ -5,9 +5,8 @@ import sys
 stderr = sys.stderr
 sys.stderr = open(os.devnull, 'w') # Patch to remove "Using TensorFlow backend" output
 from keras import backend as k
-from keras.preprocessing.image import ImageDataGenerator
+sys.stderr = stderr # Patch to remove "Using TensorFlow backend" output
 
-import env
 
 def get_video_data_from_file(path: str) -> np.ndarray:
 	video_data = np.load(path) # T x H x W x C
@@ -32,37 +31,3 @@ def reshape_video_data(video_data: np.ndarray) -> np.ndarray:
 
 def normalize_video_data(video_data: np.ndarray) -> np.ndarray:
 	return video_data.astype(np.float32) / 255
-
-def standardize(videos):
-		print("Original shape of videos: {}".format(videos.shape))
-		all_images = get_images_from_videos(videos)
-		print("Shape of images: {}".format(all_images.shape))
-		image_dg = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True)
-		image_dg.fit(all_images)
-		image_dg.standardize(all_images)
-		videos = get_videos_from_images(all_images)
-		print("Shape of videos: {}".format(videos.shape))
-		return videos
-
-
-def get_images_from_videos(videos):
-		images = np.empty([0, 100, 50, 3])
-		for video in videos:
-				images = np.concatenate((images, video), axis=0)
-
-		return images
-
-def get_videos_from_images(images):
-		total_images = np.size(images,0)
-		total_videos = total_images / env.FRAME_COUNT
-		s = 1
-		videos = []
-		while s <= total_videos:
-				images_chunk = images[:env.FRAME_COUNT * s]
-				print(np.array(videos).shape)
-				videos.append(images_chunk)
-				s += 1
-		return np.array(videos)
-
-
-
