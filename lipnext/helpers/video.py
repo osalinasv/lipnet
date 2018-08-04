4d58b508e3897e90a1c8e1912fa04c2d801926ce
Omar Adrian Salinas Villanueva <omar.salinas1611@gmail.com>
2018-08-04 00:25:50 -0600

    Optimized video data reshape

--- lipnext/helpers/video.py
+++ lipnext/helpers/video.py
@@ -18,21 +18,15 @@ def reshape_and_normalize_video_data(video_data: np.ndarray) -> np.ndarray:
 
 
 def reshape_video_data(video_data: np.ndarray) -> np.ndarray:
-	reshaped_video_data = np.array([reshape_video_frame(frame) for frame in video_data]) # T x W x H x C
+	reshaped_video_data = video_data.swapaxes(1, 2) # T x W x H x C
+
+	if len(reshaped_video_data) < 4:
+		reshaped_video_data = reshaped_video_data.swapaxes(1, 3).swapaxes(1, 2) # Add grayscale channel
 
 	if k.image_data_format() == 'channels_first':
 		reshaped_video_data = np.rollaxis(reshaped_video_data, 3) # C x T x W x H
 
 	return reshaped_video_data
-
-
-def reshape_video_frame(frame: np.ndarray) -> np.ndarray:
-	frame = frame.swapaxes(0, 1) # swap width and height to form format W x H x C
-
-	if len(frame.shape) < 3:
-		frame = np.array([frame]).swapaxes(0, 2).swapaxes(0, 1)  # Add grayscale channel
-
-	return frame
 
 
 def normalize_video_data(video_data: np.ndarray) -> np.ndarray:
