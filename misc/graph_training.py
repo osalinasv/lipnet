646a23685d536a8e83e71e0d53f057b947909430
Omar Adrian Salinas Villanueva <omar.salinas1611@gmail.com>
2018-08-31 14:59:58 -0600

    Better training graphication

--- misc/graph_training.py
+++ misc/graph_training.py
@@ -1,16 +1,25 @@
 import csv
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import numpy as np
 import os
 import sys
 
+from common.files import is_file, get_file_extension
+
+
+# set PYTHONPATH=%PYTHONPATH%;./
+# python misc\graph_training.py data\logs\2018-08-28-00-04-11\2018-08-28-00-04-11_train.csv
+
 
 # These are the "Tableau 20" colors as RGB.    
-tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
-						 (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),
-						 (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),
-						 (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),
-						 (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
+tableau20 = [
+	(31, 119, 180),  (174, 199, 232), (255, 127, 14),  (255, 187, 120),
+	(44, 160, 44),   (152, 223, 138), (214, 39, 40),   (255, 152, 150),
+	(148, 103, 189), (197, 176, 213), (140, 86, 75),   (196, 156, 148),
+	(227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),
+	(188, 189, 34),  (219, 219, 141), (23, 190, 207),  (158, 218, 229)
+]
 
 # Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts.    
 norm_rgb   = lambda x: x / 255.0
@@ -18,77 +27,119 @@ norm_color = lambda x: tuple(map(norm_rgb, x))
 
 tableau20 = list(map(norm_color, tableau20))
 
-# You typically want your plot to be ~1.33x wider than tall. This plot is a rare    
-# exception because of the number of lines being plotted on it.    
-# Common sizes: (10, 7.5) and (12, 9)
-plt.figure(figsize=(10, 7.5))
 
-ax = plt.subplot(111)
+def get_training_data(path: str) -> (np.ndarray, np.ndarray):
+	print('CSV file: {}'.format(path))
 
-# Remove the plot frame lines. They are unnecessary chartjunk.
-ax.spines["top"].set_visible(False)
-ax.spines["bottom"].set_visible(False)
-ax.spines["right"].set_visible(False)
-ax.spines["left"].set_visible(False)
+	if not is_file(path) or get_file_extension(path) != '.csv':
+		print('No valid CSV file found')
+		return
 
-# Ensure that the axis ticks only show up on the bottom and left of the plot.
-# Ticks on the right and top of the plot are generally unnecessary chartjunk.
-ax.get_xaxis().tick_bottom()
-ax.get_yaxis().tick_left()
+	with open(path, mode='r') as f:
+		reader = csv.DictReader(f, delimiter=',')
+		data = [r for r in reader]
 
-# Obtain the data from the CSV
-csv_file_path = os.path.realpath(sys.argv[1])
-print('csv file: {}\n'.format(csv_file_path))
+	loss = np.array([r['loss'] for r in data], dtype=np.float32)
+	val_loss = np.array([r['val_loss'] for r in data], dtype=np.float32)
 
-with open(csv_file_path, mode='r') as f:
-	reader = csv.DictReader(f, delimiter=',')
-	data = [r for r in reader]
+	return loss, val_loss
 
-loss = np.array([r['loss'] for r in data], dtype=np.float32)
-val_loss = np.array([r['val_loss'] for r in data], dtype=np.float32)
 
-loss_range = np.min(loss), np.max(loss)
-val_loss_range = np.min(val_loss), np.max(val_loss)
+def style_axis(ax: plt.Axes, x_max: int, y_max: float):
+	# Remove the plot frame lines. They are unnecessary chartjunk.
+	ax.spines["top"].set_visible(False)
+	ax.spines["bottom"].set_visible(False)
+	ax.spines["right"].set_visible(False)
+	ax.spines["left"].set_visible(False)
 
-y_range = min(loss_range[0], val_loss_range[0]), max(loss_range[1], val_loss_range[1])
-x_range = 0, max(len(val_loss), len(loss))
+	# Ensure that the axis ticks only show up on the bottom and left of the plot.
+	# Ticks on the right and top of the plot are generally unnecessary chartjunk.
+	ax.get_xaxis().tick_bottom()
+	ax.get_yaxis().tick_left()
 
-# Limit the range of the plot to only where the data is.
-# Avoid unnecessary whitespace.
-plt.ylim(y_range[0], y_range[1])
-plt.xlim(x_range[0], x_range[1]) 
+	ax.tick_params(axis='both', which='major', labelsize=10)
 
-plt.yticks(range(int(y_range[0]), int(y_range[1]) + 10, 10), fontsize=10)
-plt.xticks(range(int(x_range[0]), int(x_range[1]) + 10, 10), fontsize=10)
+	# Limit the range of the plot to only where the data is.
+	# Avoid unnecessary whitespace.
+	ax.set_ylim(0, y_max)
+	ax.set_xlim(0, x_max)
 
-# Provide tick lines across the plot to help your viewers trace along
-# the axis ticks. Make sure that the lines are light and small so they
-# don't obscure the primary data lines.
-helper_line_range_x = range(x_range[0], x_range[1])
+	ax.yaxis.set_ticks(np.arange(0, y_max + 5.0, 5.0))
+	ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.0f'))
 
-for y in range(int(y_range[0]), int(y_range[1]) + 10, 10):
-	plt.plot(helper_line_range_x, [y] * len(helper_line_range_x), "--", lw=0.5, color="black", alpha=0.3)
+	ax.xaxis.set_ticks(range(0, x_max + 10, 10))
 
-# Remove the tick marks; they are unnecessary with the tick lines we just plotted.
-plt.tick_params(axis="both", which="both", bottom="off", top="off", labelbottom="on", left="off", right="off", labelleft="on")
+	# Provide tick lines across the plot to help your viewers trace along
+	# the axis ticks. Make sure that the lines are light and small so they
+	# don't obscure the primary data lines.
+	ax.grid(b=True, axis='y', alpha=0.3)
 
-# Plot each line separately with its own color, using the Tableau 20
-# color set in order.
-plt.plot(loss, label='loss', lw=2.5, color=tableau20[14])
-plt.plot(val_loss, label='val_loss', lw=2.5, color=tableau20[0])
+	ax.set_ylabel('pérdida', fontsize=10, labelpad=5, weight='bold')
+	ax.set_xlabel('epoch', fontsize=10, labelpad=5, weight='bold')
 
-# Again, make sure that all labels are large enough to be easily read
-# by the viewer.
-plt.text(x_range[1] - 2, y_range[1] - 2, 'Loss', fontsize=14, color=tableau20[14], ha='right')
-plt.text(x_range[1] - 2, y_range[1] - 5, 'Validation loss', fontsize=14, color=tableau20[0], ha='right')
+	return ax
 
-plt.text(np.mean(x_range), y_range[1] + 3, "Training loss over time (Epochs)", fontsize=16, ha="center")
 
-plt.text(x_range[0], y_range[0] - 8,
-	"Author: Omar Adrian Salinas Villanueva"
-	"\nNote: Data values captured using Keras' CSVLogger and ModelCheckpoint", fontsize=8, alpha=0.6)   
+def annotate_lowest(dataset: np.ndarray, ax: plt.Axes):
+	x = np.argmin(dataset)
+	y = dataset[x]
 
-csv_name = os.path.splitext(os.path.basename(csv_file_path))[0]
-plt.savefig('{}_graph.png'.format(csv_name), bbox_inches="tight")
+	print('Found lowest value at: ({}, {:.4f})'.format(x, y))
 
-plt.show()
+	text = 'val_loss: ({}, {:.3f})'.format(x, y)
+
+	bbox_props = dict(boxstyle="round,pad=0.35", fc="w", ec="k", lw=0.72)
+	arrow_props = dict(arrowstyle="-|>", connectionstyle="angle,angleA=0,angleB=60")
+
+	kw = dict(xycoords='data',textcoords="offset pixels", arrowprops=arrow_props, bbox=bbox_props, ha="right", va="top")
+
+	ax.annotate(text, xy=(x, y), xytext=(-35, 60), **kw)
+
+
+def main():
+	csv_file_path = os.path.realpath(sys.argv[1])
+	loss, val_loss = get_training_data(csv_file_path)
+
+	if loss is None or len(loss) <= 0 or val_loss is None or len(val_loss) <= 0:
+		return
+
+	dataset = np.array([loss, val_loss])
+	x_max = max([len(x) for x in dataset])
+	y_max = np.max(dataset)
+
+	# You typically want your plot to be ~1.33x wider than tall. This plot is a rare    
+	# exception because of the number of lines being plotted on it.    
+	# Common sizes: (10, 7.5) and (12, 9)
+	plt.figure(figsize=(10, 7.5))
+
+	ax = plt.subplot(111)
+
+	# Plot each line separately with its own color, using the Tableau 20
+	# color set in order.
+	plt.plot(loss, label='loss', color=tableau20[1])
+	plt.plot(val_loss, label='val_loss', color=tableau20[0])
+
+	ax = style_axis(ax, x_max, y_max)
+
+	annotate_lowest(val_loss, ax)
+	ax.legend()
+
+	plt.text(x_max / 2.0, y_max + 5.0, "Pérdida a lo largo del tiempo de entrenamiento", fontsize=16, weight='bold', ha="center")
+
+	plt.text(0, -8,
+		"Autor: Omar Adrian Salinas Villanueva\n"
+		"Universidad Autónoma de Ciudad Juárez", fontsize=8, alpha=0.6)
+
+	current_dir = os.path.dirname(os.path.realpath(__file__))
+	csv_name = os.path.splitext(os.path.basename(csv_file_path))[0]
+
+	output_path = '{}_graph.png'.format(os.path.join(current_dir, csv_name))
+
+	plt.savefig(output_path, bbox_inches="tight")
+	print('Saved output to: {}'.format(output_path))
+
+	plt.show()
+
+
+if __name__ == '__main__':
+	main()
